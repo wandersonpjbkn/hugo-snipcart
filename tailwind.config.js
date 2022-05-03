e9f7479e8a93d4a6ff3e9bbbb1ff1922ce7eb474
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2022-05-03 20:39:50 -0300

    Added config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './content/**/*.md',
+    './layouts/**/*.html',
+    './themes/**/content/**/*.md',
+    './themes/**/layouts/**/*.html',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
